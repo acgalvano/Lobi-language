116be5235dc41c956b5bd6a2f011821ab2446f3a
acgalvano <114446968+acgalvano@users.noreply.github.com>
2024-08-20 19:26:50 -0700

    Update LobiScript.jsx Eng and Fr

--- LobiScript.jsx
+++ LobiScript.jsx
@@ -83,7 +83,7 @@ var Homepage = React.createClass(
 
 
     <h1 className='ui dividing header'>Lobi language & culture</h1>
-    <h2 >Langue et culture Lobi</h2>
+    <h2 >Lobi dɪɪrɪ</h2>
 
     <div className="ui segment">
     <img className="ui medium spaced rounded image" src="./images/Sansan_smiling.png"></img>
@@ -94,7 +94,7 @@ var Homepage = React.createClass(
   
     <p>This website is a collaboration between Sansan Claude Hien and linguists at the University of California, Berkeley. </p>
     
-    <p>This website is intended first and foremost as a resource for the Lobi people, and in addition for language-learners who are interested in learning more about the Lobi language.</p>
+    <p>This website is intended first and foremost as a resource for the Lobi people, and in addition for learners of all backgrounds who are interested in learning more about Lobi language & culture. </p>
     
     <h1 className='ui dividing header'>Project members</h1>
 
@@ -104,7 +104,7 @@ var Homepage = React.createClass(
         <p> TBD </p> 
 
     <h3> Amber Galvano </h3>
-        <p> <a href="https://ambergalvano.weebly.com/">Amber</a> is a Ph.D. student in linguistics at UC Berkeley. </p>
+        <p> <a href="https://ambergalvano.weebly.com/">Amber</a> is a Ph.D. student in Linguistics at UC Berkeley. </p>
 
       </div>
       }
@@ -118,45 +118,29 @@ var HomepageFR = React.createClass(
   return   <div className='ui text container'> 
 
 
-      <h1 className='ui dividing header'>Recueil de contes et chants Atchan </h1>
-      <h2 >ÁCAN NANMƐ LÊ ÁLƐ́BHƆ́ </h2>
+      <h1 className='ui dividing header'>Langue et culture Lobi</h1>
+      <h2 >Lobi dɪɪrɪ</h2>
 
       <div className="ui segment">
       <img className="ui medium spaced rounded image" src="./images/Anono_street.jpg"></img>
       <img className="ui medium spaced rounded image" src="./images/Blockhauss_ceremony.jpg"></img>
       </div>
 
-    <p> Ce site web fait la collection de textes, chansons et contes en langue atchan plus connue sous le nom ébrié. 
-      Cette langue est parlée par les Ébrié qui se nomment eux-mêmes Tchaman et qui vivent dans une soixantaine de villages tous situés à Abidjan (actuelle capitale de la Côte d'Ivoire) et ses alentours. 
-      Selon la classification de Williamson & Blench (2000:18), l'atchan forme, avec le nghlwa (mbatto), le sous-groupe Potou, lui-même issu du groupe Kwa appartenant à la Famille Niger-Congo. 
-      Les éléments collectés ici vous permettront d'apprendre davantage sur le peuple, sa langue et ses us et coutumes.   </p> 
+    <p> Ce site est en construction. </p> 
   
-    <p> Ce site est une collaboration entre Dr Yao Maxime Dido, Professeur de linguistique à l'Université Alassane Ouattara de Bouaké (Côte d'Ivoire) et des linguistes de l'Université de Berkeley en Californie aux USA. 
-      Dr Dido a participé à la mise en place de l'orthographe de l'atchan avec le Prof. Rémy Bôle-Richard, un linguiste spécialiste des langues Ouest-Africaines, qui en est le concepteur.</p>
-    
-    <p> C'est un site qui sert de ressource d'abord aux Tchaman, ensuite aux chercheurs qui souhaitent en savoir plus sur la langue. </p>
+    <p> Ce site Web est le fruit d'une collaboration entre Sansan Claude Hien et des linguistes de l'Université de Californie à Berkeley. </p>
+
+    <p> Ce site Web est destiné avant tout à être une ressource pour le peuple Lobi, mais également pour les apprenants de tous horizons qui souhaitent en savoir plus sur la langue et la culture Lobi. <p>
     
     <h1 className='ui dividing header'>Membres du projet</h1>
 
     {/* <h2> (Visit the Storytellers page to learn more about the people who you see and hear in the texts.) </h2> */}
 
-    <h3> Yao Maxime Dido </h3>
-        <p> Dr Dido est locuteur natif de l'atchan, originaire du village de Blockhauss et auteur d'articles publiés sur sa langue maternelle de 2018 à 2021 et prévoit de publier d'autres dans les mois à venir. </p> 
+    <h3> Sansan Claude Hien </h3>
+        <p> À déterminer </p> 
 
-    <h3> Becky Jarvis </h3>
-        <p> <a href="http://sites.google.com/berkeley.edu/rebecca-jarvis/">Becky</a> est candidate au doctorat en linguistique à l’Université de Californie à Berkeley.  </p>
-
-    <h3> Katie Russell </h3>
-        <p> <a href="http://www.krrussell.com">Katie</a> est candidate au doctorat en linguistique à l’Université de Californie à Berkeley. </p> 
-
-    <h3> Siddharth Ganapathy </h3>
-        <p> Siddharth est étudiant en informatique et linguistique à l’Université de Californie à Berkeley. </p> 
-
-    <h3> Lindsay Hatch </h3>
-        <p> Lindsay est étudiante en linguistique à l’Université de Californie à Berkeley. </p> 
-
-    <h3> Marie-Anne Xu </h3>
-        <p> Marie-Anne est étudiante en génie électrique et informatique  à l'Université de Californie à Berkeley. </p> 
+    <h3> Amber Galvano </h3>
+        <p> <a href="https://ambergalvano.weebly.com/">Amber</a> est doctorante en linguistique à l'Université de Californie à Berkeley.  </p>
 
         </div>
         }
