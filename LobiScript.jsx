27c7b520ccddf396b618b9530c5184f6fd4423b9
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2025-03-10 23:35:58 -0700

    Removed this.prop.show_IPA

--- LobiScript.jsx
+++ LobiScript.jsx
@@ -2089,7 +2089,7 @@ var Sentence = React.createClass({
     }
     
     // interlinear gloss alignment
-    if (this.props.show_gloss || this.props.show_IPA) {
+    if (this.props.show_gloss) { //|| this.props.show_IPA
       var utterances = sentence.utterance.split(' ');
       var morphemes = sentence.morphemes.split(' ');
       var glosses = sentence.gloss.split(' ');
