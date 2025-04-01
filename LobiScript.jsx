7039c5d593fa32776e3401ac0f1f2e1307940c4f
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2025-04-01 13:34:14 -0700

    Changed toggleIPA to only change IPA tier

--- LobiScript.jsx
+++ LobiScript.jsx
@@ -976,13 +976,13 @@ var StoryViewFR = React.createClass({
   },
   //toggles IPA or not
   toggleIPA: function() {
-    var new_show_gloss = !this.state.show_gloss;
+    var new_show_IPA = !this.state.show_IPA;
     var new_story_view = this.state.story_view;
-    if(new_show_gloss) {
+    if(new_show_gloss || new_show_IPA) {
       new_story_view = false;
     }
-    this.setState({show_IPA: this.state.show_IPA, 
-                   show_gloss: new_show_gloss,
+    this.setState({show_IPA: new_show_IPA, 
+                   show_gloss: this.state.show_gloss,
                     story_view: new_story_view});
     // var new_show_IPA = !this.state.show_IPA;
     // var new_story_view = this.state.story_view;
@@ -997,7 +997,7 @@ var StoryViewFR = React.createClass({
   toggleGloss: function() {
     var new_show_gloss = !this.state.show_gloss;
     var new_story_view = this.state.story_view;
-    if(new_show_gloss) {
+    if(new_show_gloss || new_show_IPA) {
       new_story_view = false;
     }
     this.setState({show_IPA: this.state.show_IPA, 
