9623166e4839f7088a9abcba5f46b5c9c4625a5a
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2025-04-01 13:19:37 -0700

    Changed or to ||, should fix website crash

--- LobiScript.jsx
+++ LobiScript.jsx
@@ -770,7 +770,7 @@ var StoryView = React.createClass({
   toggleIPA: function() {
     var new_show_IPA = !this.state.show_IPA;
     var new_story_view = this.state.story_view;
-    if(new_show_gloss or new_show_IPA) {
+    if(new_show_gloss || new_show_IPA) {
       new_story_view = false;
     }
     this.setState({show_IPA: new_show_IPA,
@@ -781,7 +781,7 @@ var StoryView = React.createClass({
   toggleGloss: function() {
     var new_show_gloss = !this.state.show_gloss;
     var new_story_view = this.state.story_view;
-    if(new_show_gloss or new_show_IPA) {
+    if(new_show_gloss || new_show_IPA) {
       new_story_view = false;
     }
     this.setState({show_IPA: this.state.show_IPA,
