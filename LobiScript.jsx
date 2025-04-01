0be1bd39ff1048d52a24c4ee48cbf7ed35656bbd
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2025-04-01 13:48:37 -0700

    Removed toggle updates to storyView

--- LobiScript.jsx
+++ LobiScript.jsx
@@ -769,10 +769,6 @@ var StoryView = React.createClass({
   //toggles IPA or not
   toggleIPA: function() {
     var new_show_IPA = !this.state.show_IPA;
-    var new_story_view = this.state.story_view;
-    if(new_show_gloss || new_show_IPA) {
-      new_story_view = false;
-    }
     this.setState({show_IPA: new_show_IPA,
                    show_gloss: this.state.show_gloss,
                     story_view: this.state.story_view});
@@ -780,10 +776,6 @@ var StoryView = React.createClass({
   //toggles interlinear gloss or not
   toggleGloss: function() {
     var new_show_gloss = !this.state.show_gloss;
-    var new_story_view = this.state.story_view;
-    if(new_show_gloss || new_show_IPA) {
-      new_story_view = false;
-    }
     this.setState({show_IPA: this.state.show_IPA,
                    show_gloss: new_show_gloss,
                     story_view: this.state.story_view});
@@ -977,10 +969,6 @@ var StoryViewFR = React.createClass({
   //toggles IPA or not
   toggleIPA: function() {
     var new_show_IPA = !this.state.show_IPA;
-    var new_story_view = this.state.story_view;
-    if(new_show_gloss || new_show_IPA) {
-      new_story_view = false;
-    }
     this.setState({show_IPA: new_show_IPA, 
                    show_gloss: this.state.show_gloss,
                     story_view: this.state.story_view});
@@ -988,10 +976,6 @@ var StoryViewFR = React.createClass({
   //toggles interlinear gloss or not
   toggleGloss: function() {
     var new_show_gloss = !this.state.show_gloss;
-    var new_story_view = this.state.story_view;
-    if(new_show_gloss || new_show_IPA) {
-      new_story_view = false;
-    }
     this.setState({show_IPA: this.state.show_IPA, 
                    show_gloss: new_show_gloss,
                     story_view: this.state.story_view});
