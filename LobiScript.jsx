c6411159e1a8ca66cdbb5ca967c52f473870caa4
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2025-04-22 13:08:56 -0700

    Adding morphemes only with no toggle

--- LobiScript.jsx
+++ LobiScript.jsx
@@ -2134,6 +2134,9 @@ var Sentence = React.createClass({
       }.bind(this)).value();
       //utterance = <span>{utterances}<br/></span>;
       gloss = <span>{glosses}<br/></span>;
+    } else {
+      glosses = sentence.morphemes.split(' ');
+      gloss = <span>{glosses}<br/></span>;
     }
     const translation = global_show_french ? sentence.french : sentence.translation;
 
